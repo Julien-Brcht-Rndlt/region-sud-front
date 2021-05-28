6e94b964228e207ff7f200cf945e4d5bbe719de8
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-05-28 12:10:40 +0200

    cleaning conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import theme from './styles/theme';
+
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <h1>Region-Sud</h1>
+      </ThemeProvider>
+    </>
   );
 }
 
