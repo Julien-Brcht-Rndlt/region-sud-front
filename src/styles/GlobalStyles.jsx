c37fe60484758e8b398f2669fe5de9c799901310
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-05-31 17:19:41 +0200

    adding colors, fonts and responsvive breakpoints to theme

--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -4,8 +4,6 @@ const GlobalStyles = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    background: teal;
-    font-family: Open-Sans, Helvetica, Sans-Serif;
   }
 `;
 
--- src/styles/theme.jsx
+++ src/styles/theme.jsx
@@ -1,1 +1,51 @@
-export default function theme() {}
+const fontWeightNormal = 400;
+const fontFamilyNormal = "'Dosis',sans-serif'";
+const fontWeightLogo = 400;
+const fontFamilyLogo = "'Nunito',sans-serif'";
+const fontWeightFeature = 700;
+const fontFamilyFeature = 'Fira Sans';
+const mainTextColor = '#000';
+
+// setup default theme
+
+export const theme = {
+  // fonts theme
+  fonts: {
+    fontWeightNormal,
+    fontFamilyNormal,
+    fontWeightLogo,
+    fontFamilyLogo,
+    fontWeightFeature,
+    fontFamilyFeature,
+    mainTextColor,
+  },
+  // colors theme
+  colors: {
+    blueRegionSud: '005aff',
+    whiteRegionSud: 'ffffff',
+    yellowRegionSud: 'feca5a',
+  },
+};
+
+// setup responsive breakpoints
+
+const size = {
+  mobileS: '320px',
+  mobileM: '375px',
+  mobileL: '425px',
+  tablet: '768px',
+  laptop: '1024px',
+  laptopL: '1440px',
+  desktop: '2560px',
+};
+
+export const device = {
+  mobileS: `(min-width: ${size.mobileS})`,
+  mobileM: `(min-width: ${size.mobileM})`,
+  mobileL: `(min-width: ${size.mobileL})`,
+  tablet: `(min-width: ${size.tablet})`,
+  laptop: `(min-width: ${size.laptop})`,
+  laptopL: `(min-width: ${size.laptop})`,
+  desktop: `(min-width: ${size.desktop})`,
+  desktopL: `(min-width: ${size.desktop})`,
+};
