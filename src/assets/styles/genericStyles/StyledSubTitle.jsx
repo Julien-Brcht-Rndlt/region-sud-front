22aacbd1dfb26c723b09767586b1c59c036bac6f
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-07 17:07:51 +0200

    improving StyledSubTitle for h2 title (passing props)

--- src/assets/styles/genericStyles/StyledSubTitle.jsx
+++ src/assets/styles/genericStyles/StyledSubTitle.jsx
@@ -1,9 +1,16 @@
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-export const StyledSubTitle = styled.h2`
+export const StyledSubTitleH2 = styled.h2`
   font-family: ${(props) => props.theme.fontFamilyFeature};
   font-weight: ${(props) => props.theme.fontWeightBold};
   color: ${(props) => props.theme.blueFeatureColor};
 `;
 
-export const fake = styled.div``;
+export default function StyledSubTitle({ subtitle }) {
+  return <StyledSubTitleH2>{subtitle}</StyledSubTitleH2>;
+}
+
+StyledSubTitle.propTypes = {
+  subtitle: PropTypes.string.isRequired,
+};
