3b6f3b6b758db282318d25f734a8166ded92a552
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-07 16:48:45 +0200

    creating StyledSubTitle for h2 title

---
+++ src/assets/styles/genericStyles/StyledSubTitle.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledSubTitle = styled.h2`
+  font-family: ${(props) => props.theme.fontFamilyFeature};
+  font-weight: ${(props) => props.theme.fontWeightBold};
+  color: ${(props) => props.theme.blueFeatureColor};
+`;
+
+export const fake = styled.div``;
