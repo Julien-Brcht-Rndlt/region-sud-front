5c8351edccc54b1513158f64f08e21cf0096a084
[violaine ernotte] <[e.violaine@yahoo.fr]>
2021-06-08 16:43:32 +0200

    creation folder utils with dum data

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './assets/styles/GlobalStyles';
 import RouterPaf from './RouterPaf';
 import theme from './assets/styles/theme';
+import FunnelContext from './components/context/funnel-context';
 
 function App() {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <RouterPaf />
-      </ThemeProvider>
+      <FunnelContext.Provider>
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          <RouterPaf />
+        </ThemeProvider>
+      </FunnelContext.Provider>
     </>
   );
 }
