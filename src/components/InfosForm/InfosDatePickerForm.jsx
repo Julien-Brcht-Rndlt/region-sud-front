e67adca50b23f1424acf1ff7935ea7e7c44ed814
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-11 10:53:58 +0200

    put back InfosDatePickerForm in InfosForm file

--- src/components/InfosForm/InfosDatePickerForm.jsx
+++ src/components/InfosForm/InfosDatePickerForm.jsx
@@ -2,18 +2,36 @@ import { useState } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
 import {
   StyledFormItem,
   StyledFormItemLabel,
 } from '../../assets/styles/genericStyles/StyledFormItem';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const StyledDatePickerInfosForm = styled(DatePicker)`
   padding: 15px;
   border: 1px solid ${(props) => props.theme.blueFeatureColor};
 `;
 
+/* const initialState = {
+  startDate: null,
+  endDate: null,
+  focusedInput: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'focusChange':
+      return { ...state, focusedInput: action.payload };
+    case 'dateChange':
+      return action.payload;
+    default:
+      throw new Error();
+  }
+} */
+
 export default function InfosDatePickerForm({ label }) {
+  /* const [state, dispatch] = useReducer(reducer, initialState); */
   const [selectedDate, setSelectedDate] = useState(new Date());
 
   const handleChange = (date) => {
@@ -37,9 +55,15 @@ export default function InfosDatePickerForm({ label }) {
 }
 
 InfosDatePickerForm.propTypes = {
-  label: PropTypes.string,
+  label: PropTypes.string.isRequired,
 };
 
-InfosDatePickerForm.defaultProps = {
-  label: '',
-};
+/*  <DateRangeInput
+      onDatesChange={(data) => dispatch({ type: 'dateChange', payload: data })}
+      onFocusChange={(focusedInput) =>
+        dispatch({ type: 'focusChange', payload: focusedInput })
+      }
+      startDate={state.startDate} // Date or null
+      endDate={state.endDate} // Date or null
+      focusedInput={state.focusedInput} // START_DATE, END_DATE or null
+    /> */
