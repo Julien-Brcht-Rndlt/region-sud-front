fab3d9553809aa92886d177e00e7d409221de95a
TouyardK <touyard.kevin@outlook.fr>
2021-06-15 14:11:36 +0200

    Fix align items Navbar

--- src/components/header/NavBarItem.jsx
+++ src/components/header/NavBarItem.jsx
@@ -1,15 +1,22 @@
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { StyledLink } from '../../styles/StyledNavBar';
 
+export const StyledNavBarItem = styled.div`
+  display: inline-block;
+`;
+
 export default function NavBarItem({ id, content, path }) {
   return (
-    <ul>
-      <li>
-        <StyledLink key={id} to={path}>
-          {content}
-        </StyledLink>
-      </li>
-    </ul>
+    <StyledNavBarItem>
+      <ul>
+        <li>
+          <StyledLink key={id} to={path}>
+            {content}
+          </StyledLink>
+        </li>
+      </ul>
+    </StyledNavBarItem>
   );
 }
 
