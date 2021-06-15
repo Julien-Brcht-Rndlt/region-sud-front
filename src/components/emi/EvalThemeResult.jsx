e84a0dab9806e8be51c6c2ec200f39af7a950d17
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-15 13:14:41 +0200

    creating EvalThemeResult componant parent of EvalThemeShouldList comp

---
+++ src/components/emi/EvalThemeResult.jsx
@@ -0,0 +1,10 @@
+import EvalThemeShouldList from './EvalThemeShouldList';
+
+export default function EvalThemeResult() {
+    return (
+      <div>
+        <EvalThemeShouldList />
+        <p>Vous pourrez voir toutes les recommandations à la fin de l&#39;évaluation.</p>
+      </div>
+    );
+}
