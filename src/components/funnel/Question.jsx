c8984ef551dcf770b11db306214e00da6515adf6
[violaine ernotte] <[e.violaine@yahoo.fr]>
2021-06-16 20:54:07 +0200

    responsive funnel :question, theme

--- src/components/funnel/Question.jsx
+++ src/components/funnel/Question.jsx
@@ -3,15 +3,50 @@ import PropTypes from 'prop-types';
 import AnswerList from './AnswerList';
 import { StyledTitleH3 } from '../../styles/generics/GenericTitles';
 import { Block } from '../../styles/generics/GenericContainers';
+import { device } from '../../styles/theme';
 
 export const StyledQuestionContainer = styled(Block)`
-  background-color: #e7ecf4;
-  height: 210px;
-  margin-bottom:30px;
-  margin-left:40px;
-  width:60%;
-  padding:15px;
-`;
+     
+     @media ${device.mobileS}{
+        background-color: #e7ecf4;
+        margin-bottom:10px;
+        margin-left:10px;
+        width:290px;
+        padding:10px;
+    }
+
+      @media ${device.mobileL}{
+          background-color: #e7ecf4;
+          margin-bottom:12px;
+          margin-left:12px;
+          width:380px;
+          padding:15px;
+      }
+
+      @media ${device.tablet}{
+          background-color: #e7ecf4;
+          margin-bottom:15px;
+          margin-left:15px;
+          width:700px;
+          padding:15px;
+      }
+
+      @media ${device.laptop}{
+        background-color: #e7ecf4;
+        margin-bottom:30px;
+        margin-left:40px;
+        width:900px;
+        padding:25px;
+    }
+
+      @media ${device.desktop}{
+          background-color: #e7ecf4;
+          margin-bottom:50px;
+          margin-left:40px;
+          width:1200px;
+          padding:45px;
+      }
+    `;
 
 export default function Question({ title, answers }) {
   return (
--- src/components/funnel/Theme.jsx
+++ src/components/funnel/Theme.jsx
@@ -6,6 +6,7 @@ import { Flex, FlexCol } from '../../styles/generics/GenericContainers';
 import { IconeImg } from '../../styles/generics/GenericComponents';
 import { StyledTitleH1, StyledTitleH4 } from '../../styles/generics/GenericTitles';
 import help from '../../assets/img/help.png';
+import { device } from '../../styles/theme';
 
 export const ThemeContainer = styled.div`
   background-color: #ffffff;
@@ -18,9 +19,19 @@ export const QuestionListContainer = styled.div`
 `;
 
 export const StyledTitleTheme = styled(StyledTitleH1)`
+  @media ${device.mobileS}{
+    font-size: 1.2rem;
+  }
+  
+  @media ${device.tablet}{
+    font-size: 1.7rem;
+  }
+  
+  @media ${device.laptop}{
   color: ${(props) => props.theme.blueFeatureColor};
   margin-left: 40px;
   font-size: 1.9rem;
+  }
 `;
 
 export const StyledSubtitleTheme = styled(StyledTitleH4)`
@@ -35,22 +46,53 @@ export const MicroImg = styled.img`
 `;
 
 export const ContainersubtitleTheme = styled(Flex)`
+  @media ${device.mobileS}{
+    margin-left: 5px;
+    margin-top: 5px;
+  }
+
+  @media ${device.mobileL}{
+    margin-left: 10px;
+    margin-top: 10px;
+  }
+
+  @media ${device.laptop}{
   margin-left: 40px;
   margin-top: 20px;
-`;
+  }
+  `;
 
 export const StyledBorderYellow = styled.div`
-  display: flex;
+
+  @media ${device.mobileS}{
+    display:none;
+  }
+
+  @media ${device.mobileL}{
+    display:none;
+  }
+
+  @media ${device.laptop}{
+    display: flex;
   border-bottom: 10px solid ${(props) => props.theme.yellowFeatureColor};
   width: 50px;
   padding-right: 30px;
   margin: 65px;
   margin-left: 45px;
   position: absolute;
+  }  
 `;
+
 export const StyledContainerYellow = styled.div`
+
+@media ${device.mobileS}{
+  padding-left: 5px;
+  }
+
+@media ${device.laptop}{ 
   display: flex;
-  padding: 15px;
+  padding-left: 10px;
+}
 `;
 
 export default function Theme({ title, questions, icon }) {
