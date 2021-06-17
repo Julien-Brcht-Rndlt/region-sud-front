56b60b30afc3cdb5f928932c8f53bdaf5c6bfc6c
[violaine ernotte] <[e.violaine@yahoo.fr]>
2021-06-17 15:41:27 +0200

    pb youtube

--- src/components/requirements/RequireYoutube.jsx
+++ src/components/requirements/RequireYoutube.jsx
@@ -1,10 +1,10 @@
 import YouTube from 'react-youtube';
 import styled from 'styled-components';
-import { FlexCol } from '../../styles/generics/GenericContainers';
+//  import { FlexCol } from '../../styles/generics/GenericContainers';
 import { StyledRoundedButton } from '../../styles/generics/GenericButtons';
 import { StyledTitleH4 } from '../../styles/generics/GenericTitles';
 
-export const StyledYoutubeBox = styled(FlexCol)`
+export const StyledYoutubeBox = styled.div`
   width: auto;
   height: auto;
   padding: 140px;
@@ -12,11 +12,16 @@ export const StyledYoutubeBox = styled(FlexCol)`
   padding-left: 100px;
 `;
 
+const opts = {
+   height: '430',
+   width: '360',
+};
+
 export default function RequireYoutube() {
   return (
     <StyledYoutubeBox hline="center">
       <StyledTitleH4>Comment cela va se passer :</StyledTitleH4>
-      <YouTube videoId="aP0_xJE_bgU" />
+      <YouTube opts={opts} videoId="aP0_xJE_bgU" />
       <StyledRoundedButton glassy>Voir les questions fréquentes</StyledRoundedButton>
     </StyledYoutubeBox>
   );
