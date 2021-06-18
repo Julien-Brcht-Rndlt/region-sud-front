8252ff71e4288a83e30e24f0235d0e0d0cb4057e
[violaine ernotte] <[e.violaine@yahoo.fr]>
2021-06-18 12:27:45 +0200

    responsive on Home page in progress

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,14 +3,30 @@ import ImageHome from './ImageHome';
 import TitleHome from './TitleHome';
 import MapHome from './MapHome';
 import Testimonials from './Testimonials';
+import { device } from '../../styles/theme';
+import { FlexCol, Flex } from '../../styles/generics/GenericContainers';
 
-export const StyledHome = styled.div`
-  display: flex;
+export const StyledHome = styled(Flex)`
+ @media ${device.mobileS}{
+    flex-direction: column;
+  }
+  @media ${device.tablet}{
+    flex-direction:row;
+    justify-content: space-around;
+    align-items: stretch;
+    align-content: space-around;
+  }
+  
+  @media ${device.desktop}{
+  flex-direction:row;
   justify-content: space-around;
   align-items: stretch;
   align-content: space-around;
+  }
 `;
-export const WrapperHome = styled.div``;
+
+export const WrapperHome = styled(FlexCol)`
+  `;
 
 export default function Home() {
   return (
