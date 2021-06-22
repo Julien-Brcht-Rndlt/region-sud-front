70b4858f4105de18ab5e281aeab80791a8cd9fda
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-22 12:04:43 +0200

    adding pic/scoring div area

--- src/components/emi/EvalGlobalScore.jsx
+++ src/components/emi/EvalGlobalScore.jsx
@@ -6,12 +6,12 @@ export default function EvalGlobalScore() {
       <div>
         <div>
           <StyledTitleH2>Mon résultat</StyledTitleH2>
-
+          <div>Afficher le score</div>
           <StyledButton>Envoyer par mail</StyledButton>
           <StyledButton glassy>Imprimer</StyledButton>
         </div>
         <div>
-          <div>Diplay recap icones</div>
+          <div>Afficher recap icones</div>
           <div>liste des reco en fonction des icones</div>
         </div>
         <StyledButton glassy>En savoir +</StyledButton>
