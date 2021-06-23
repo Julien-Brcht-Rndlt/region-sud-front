69c462fe39a30ddf2617eda3d637003556a4b90f
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-23 12:29:28 +0200

    applying score representation system to EvalThemeScoring component

--- src/components/emi/EvalThemeScoring.jsx
+++ src/components/emi/EvalThemeScoring.jsx
@@ -1,8 +1,9 @@
 import { useContext } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import EvalContext from '../../contexts/EvalContext';
 import { StyledTitleH3, StyledTitleH4 } from '../../styles/generics/GenericTitles';
+import EvalContext from '../../contexts/EvalContext';
+import Scoring from './scoring.json';
 
 export const StyledSubtitleH4 = styled.h4`
   color: ${(props) => props.theme.blueFeatureColor};
@@ -10,22 +11,26 @@ export const StyledSubtitleH4 = styled.h4`
   font-size: 1rem;
 `;
 
-export default function EvalThemeScoring({ themeTitle, themeScore, scoreIcone }) {
+export default function EvalThemeScoring({ themeId, themeTitle }) {
   const { evalContext } = useContext(EvalContext);
-  evalContext.score = 0;
+  const { score } = evalContext;
+
+  const scoresReprs = Scoring.themes_scoring[themeId - 1].scores_reprs;
+  scoresReprs.filter((scoreRepr) => scoreRepr.min <= score && scoreRepr.max >= score);
+  const [scoreRepr] = scoresReprs;
+
     return (
       <div>
         <StyledTitleH3>Votre indice</StyledTitleH3>
         <StyledTitleH4>{themeTitle}</StyledTitleH4>
         {
-          themeScore > 0 && <div><img src={scoreIcone} alt="" /></div>
+          scoreRepr && <div><img src={scoreRepr.icone} alt="" /></div>
         }
       </div>
     );
 }
 
 EvalThemeScoring.propTypes = {
+    themeId: PropTypes.number.isRequired,
     themeTitle: PropTypes.string.isRequired,
-    themeScore: PropTypes.number.isRequired,
-    scoreIcone: PropTypes.string.isRequired,
 };
