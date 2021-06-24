85cce9b2641977dcbcc62f933a35e1bf87b22b7f
TouyardK <touyard.kevin@outlook.fr>
2021-06-24 14:28:03 +0200

    intégration de la modal pour la liste des documents a fournir

--- src/EmiRouter.jsx
+++ src/EmiRouter.jsx
@@ -5,6 +5,7 @@ import Footer from './components/footer/Footer';
 import EvaluateImpact from './components/emi/EvaluateImpact';
 import FaqHome from './components/faq/FaqHome';
 import EvalResult from './components/emi/EvalResult';
+import RequireModal from './components/requirements/RequireModal';
 
 export default function EmiRouter() {
   return (
@@ -15,6 +16,7 @@ export default function EmiRouter() {
         <Route path="/EmiHome" component={EvaluateImpact} />
         <Route path="/FaqHome" component={FaqHome} />
         <Route path="/EmiResult" component={EvalResult} />
+        <Route path="/RequireModal" component={RequireModal} />
       </Switch>
       <Footer />
     </Router>
