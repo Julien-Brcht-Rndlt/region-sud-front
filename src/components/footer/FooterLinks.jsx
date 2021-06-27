99e86ddf2c29582b5b25b38bc9d4b6258f2d1aea
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-27 23:55:41 +0200

    fixing warnings key map

--- src/components/footer/FooterLinks.jsx
+++ src/components/footer/FooterLinks.jsx
@@ -2,19 +2,28 @@ import PropTypes from 'prop-types';
 import { FooterLinksList, FooterLinksALink } from '../../styles/StyledFooter';
 
 export default function FooterLinks({ links = [] }) {
+  let count = 0;
   return (
     <FooterLinksList>
-      {links.map((link) => (
-        <li>
-          <FooterLinksALink href={link.url}>{link.label}</FooterLinksALink>
-        </li>
-      ))}
+      {
+        links.map((link) => {
+          count += 1;
+          return (
+            <li>
+              <FooterLinksALink key={`${link.label}${count}`} href={link.url}>{link.label}</FooterLinksALink>
+            </li>
+          );
+        })
+      }
     </FooterLinksList>
   );
 }
 
 FooterLinks.propTypes = {
-  links: PropTypes.arrayOf(PropTypes.string),
+  links: PropTypes.arrayOf(PropTypes.shape({
+    label: PropTypes.string,
+    url: PropTypes.string,
+  })),
 };
 
 FooterLinks.defaultProps = {
