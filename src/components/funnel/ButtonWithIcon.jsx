548ce62a7bb2df5662e8d8abf591d96561f9da13
TouyardK <touyard.kevin@outlook.fr>
2021-07-06 17:20:59 +0200

    Display modal with button help

--- src/components/funnel/ButtonWithIcon.jsx
+++ src/components/funnel/ButtonWithIcon.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { StyledButton } from '../../styles/generics/GenericButtons';
 import { ReactComponent as Icon } from '../../assets/img/helphover.svg';
 import { Flex } from '../../styles/generics/GenericContainers';
+import EmiFaqModal from '../faq/EmiFaqModal';
 
 export const CompIconText = styled(Flex)`
   align-items: center;
@@ -19,15 +20,19 @@ const IconWrapper = styled.div`
 
 export default function ButtonHelp() {
   const [color, setColor] = useState('blue');
+  const [show, setShow] = useState(false);
 
   return (
-    <StyledButton glassy onMouseOver={() => setColor('#ffffff')} onMouseLeave={() => setColor('#005aff')}>
-      <CompIconText>
-        <IconWrapper fill={color}>
-          <Icon />
-        </IconWrapper>
-        Je ne m&#39;en sors pas !
-      </CompIconText>
-    </StyledButton>
+    <>
+      <StyledButton glassy onMouseOver={() => setColor('#ffffff')} onMouseLeave={() => setColor('#005aff')} onClick={() => setShow(true)}>
+        <CompIconText>
+          <IconWrapper fill={color}>
+            <Icon />
+          </IconWrapper>
+          Je ne m&#39;en sors pas !
+        </CompIconText>
+      </StyledButton>
+      <EmiFaqModal show={show} setShow={setShow} />
+    </>
   );
 }
