1a3a99b2552ff3bf7077ee58b6a42257dd99474d
[violaine ernotte] <[e.violaine@yahoo.fr]>
2021-07-07 19:00:38 +0200

    dans l'enfer du component

--- src/components/emi/ShouldGlobalComp.jsx
+++ src/components/emi/ShouldGlobalComp.jsx
@@ -1,11 +1,9 @@
 import { useState } from 'react';
 import styled from 'styled-components';
-import { FlexCol, Flex } from '../../styles/generics/GenericContainers';
-import soleil from '../../assets/img/soleil.PNG';
-import vent from '../../assets/img/vent.PNG';
-import pluie from '../../assets/img/pluie.PNG';
-import orageux from '../../assets/img/orageux.PNG';
-import { ImgProgressBar } from './QuickEvalNav';
+import PropTypes from 'prop-types';
+import { Flex, Block } from '../../styles/generics/GenericContainers';
+import { StyledTitleP } from '../../styles/generics/GenericTitles';
+/*
 import themea from '../../assets/img-temp/themea.png';
 import theme1 from '../../assets/img-temp/theme1.png';
 import theme2 from '../../assets/img-temp/theme2.png';
@@ -13,9 +11,9 @@ import theme3 from '../../assets/img-temp/theme3.png';
 import theme4 from '../../assets/img-temp/theme4.png';
 import theme5 from '../../assets/img-temp/theme5.png';
 import theme6 from '../../assets/img-temp/theme6.png';
-import theme7 from '../../assets/img-temp/theme7.png';
-//  import should from '../../mockdata/should.json';
-import ContainerGlobalRecoParagraph from './ContainerGlobalRecoParagraph';
+import theme7 from '../../assets/img-temp/theme7.png';  */
+import Should from '../../mockdata/should.json';
+import Scoring from './scoring.json';
 
 const GlobalReco = styled(Flex)`
 padding-top:20px;
@@ -23,68 +21,76 @@ flex-direction: row;
 display:flex;
 `;
 
-const MiniContainerReco = styled(FlexCol)`
-width:100px;
+const StyledScore = styled(Block)`
+height:50px;
+margin:10px;
 `;
 
-const ThemeReco = styled.img`
-width: 30px;
-height: 30px;
-padding-left:20px;
-`;
-
-export default function ShouldGlobalComp() {
-  const should = [
-          {
-              id: 0,
-              content: 'content 0 blablablabla',
-          },
-  ];
-
-  const [seeShould, setSeeShould] = useState(false);
-/*  const [containShould] = useState({should}); */
-  function handleClickShould() {
-    setSeeShould(!seeShould);
+const DisplayThemesScores = ({ handleClickTheme, themeId }) => {
+  const { score } = Should.find((item) => item.id === themeId);
+  let scoreRepr = null;
+  if (Scoring.themes_scoring.find((scoring) => scoring.id === themeId)) {
+    const themeScoring = Scoring.themes_scoring.find((scoring) => scoring.id === themeId);
+    const scoresReprs = themeScoring.scores_reprs;
+    scoreRepr = scoresReprs.find((item) => item.min <= score && item.max >= score);
   }
 
   return (
-    <>
-      <GlobalReco>
-        <MiniContainerReco>
-          <ThemeReco src={themea} alt="themea" />
-          <ImgProgressBar src={soleil} alt="sun" onClick={handleClickShould} />
-          {seeShould ? setSeeShould : should.filter((shouldItem) => shouldItem.id === 0)
-            .map((content) => <ContainerGlobalRecoParagraph {...content} />)}
-        </MiniContainerReco>
-        <MiniContainerReco>
-          <ThemeReco src={theme1} alt="theme1" />
-          <ImgProgressBar src={vent} alt="sun" />
-        </MiniContainerReco>
-        <MiniContainerReco>
-          <ThemeReco src={theme2} alt="theme2" />
-          <ImgProgressBar src={soleil} alt="sun" />
-        </MiniContainerReco>
-        <MiniContainerReco>
-          <ThemeReco src={theme3} alt="theme3" />
-          <ImgProgressBar src={pluie} alt="sun" />
-        </MiniContainerReco>
-        <MiniContainerReco>
-          <ThemeReco src={theme4} alt="theme4" />
-          <ImgProgressBar src={soleil} alt="sun" />
-        </MiniContainerReco>
-        <MiniContainerReco>
-          <ThemeReco src={theme5} alt="theme5" />
-          <ImgProgressBar src={orageux} alt="sun" />
-        </MiniContainerReco>
-        <MiniContainerReco>
-          <ThemeReco src={theme6} alt="theme6" />
-          <ImgProgressBar src={soleil} alt="sun" />
-        </MiniContainerReco>
-        <MiniContainerReco>
-          <ThemeReco src={theme7} alt="theme7" />
-          <ImgProgressBar src={soleil} alt="sun" />
-        </MiniContainerReco>
-      </GlobalReco>
-    </>
-    );
-   }
+    <button type="button" onClick={() => handleClickTheme(themeId)}><img src={scoreRepr.icone} alt={`score-${score}`} /></button>
+  );
+};
+
+const DisplayRecoList = ({ should, themeId }) => (
+  <div>
+    <p>
+      recommandation du thème :
+      {themeId}
+    </p>
+    {
+      should.map((item) => (
+        <div>
+          <StyledTitleP>{item.content}</StyledTitleP>
+          <a href={item.url}>{item.url}</a>
+        </div>
+        ))
+      }
+  </div>
+  );
+
+export default function ShouldGlobalComp() {
+  const [currId, setCurrId] = useState(0);
+  const [should, setShould] = useState([]);
+
+  const handleClickTheme = (id) => {
+    setCurrId(parseInt(id, 10));
+    const shouldList = Should.find((item) => item.id === id).should;
+    setShould(shouldList);
+  };
+
+  return (
+    <GlobalReco>
+      {
+        Should.map((item) => (
+          <StyledScore>
+            <DisplayThemesScores
+              key={item.id}
+              handleClickTheme={handleClickTheme}
+              themeId={item.id}
+            />
+          </StyledScore>
+          ))
+      }
+      <DisplayRecoList should={should} themeId={currId} />
+    </GlobalReco>
+  );
+}
+
+DisplayThemesScores.propTypes = {
+  themeId: PropTypes.number.isRequired,
+  handleClickTheme: PropTypes.func.isRequired,
+};
+
+DisplayRecoList.propTypes = {
+  themeId: PropTypes.number.isRequired,
+  should: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
